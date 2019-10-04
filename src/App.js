2fe9816f8e20ce499e3c1789603199c03933c188
Daniel Corcoran <danielcorcoran@Daniel-Corcorans-Macbook.local>
2019-10-04 13:33:25 +1000

    asdfasdf

--- src/App.js
+++ src/App.js
@@ -1,49 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
+import faker from 'faker';
 import './App.css';
+import comment from './icons/comment.png';
+import reaction from './icons/reaction.png';
+
+
+function Card () {
+    return(
+    <article className="card card-shadow">
+        <p className="title--medium">{faker.lorem.text().slice(0, 20)}</p>
+        <p>{ faker.lorem.sentence()}</p>
+    </article>)
+}
+
+function LargeCard () {
+    return(
+    <article class="card large-card card-shadow">
+        <p className="title--large">{faker.lorem.text().slice(0, 20)}</p>
+        <p className="posted-by">posted by { faker.name.firstName() }</p>
+        <p>{ faker.lorem.sentences(5) + '... '}</p>
+        <div className="card-bottom">
+            <article className="flex-container">
+                <div className="centered">
+                    <img className="image" src={comment} alt=""/>
+                    <span className="card-metric">{ faker.random.number(500) }</span>
+                </div>
+                <div className="centered">
+                    <img className="image" src={reaction} alt=""/>
+                    <span className="card-metric">{ faker.random.number(500) }</span>
+                </div>
+            </article>
+            <article className="flex-container">
+                <div className="centered">
+                    <span className="card-metric">{faker.random.number(10) + ' minute read.'}</span>
+                </div>
+                <div className="centered">
+                    <button className="button">SAVE</button>
+                </div>
+            </article>
+        </div>
+    </article>)
+}
 
 function App() {
+  console.log(faker)
   return (
       <div>
           <nav>
-              <div class="container">
-                  <h1 class="title--large">Ipsum in ut ex excepteur labore sint.</h1>
+              <div className="container">
+                  <section className="banner">
+                    <h1 className="title--huge">{faker.lorem.sentence().slice(0, 30)}</h1>
+                  </section>
               </div>
           </nav>
 
           <main>
-              <div class="container">
+              <div className="container">
                   <section class="main-container">
-                          <aside class="left-side container__general">
-                              <article class="card">
-                                  <h3>Dolore labore.</h3>
-                                  <p>Consectetur in dolor deserunt qui sint et nisi magna.</p>
-                              </article>
-                              <article class="card">
-                                  <h3>Dolore labore.</h3>
-                                  <p>Consectetur in dolor deserunt qui sint et nisi magna.</p>
-                              </article>
-                              <article class="card">
-                                  <h3>Dolore labore.</h3>
-                                  <p>Consectetur in dolor deserunt qui sint et nisi magna.</p>
-                              </article>
+                          <aside className="left-side container__general">
+                              { new Array(10).fill(true).map(item => <Card/>) }
                           </aside>
-                          <section class="article-list container__general">
-                              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam eos ipsa voluptate dolorem magni repellat nisi soluta, eaque odit in deleniti dolores, vel facere velit officiis. Nihil hic obcaecati debitis.
+                          <section className="article-list container__general">
+                              { new Array(30).fill(1).map(item => <LargeCard/>) }
                           </section>
-                          <aside class="right-side container__general">
-                              <article class="card">
-                                  <h3>Dolore labore.</h3>
-                                  <p>Consectetur in dolor deserunt qui sint et nisi magna.</p>
-                              </article>
-                              <article class="card">
-                                  <h3>Dolore labore.</h3>
-                                  <p>Consectetur in dolor deserunt qui sint et nisi magna.</p>
-                              </article>
-                              <article class="card">
-                                  <h3>Dolore labore.</h3>
-                                  <p>Consectetur in dolor deserunt qui sint et nisi magna.</p>
-                              </article>
+                          <aside className="right-side container__general">
+                              { new Array(10).fill(true).map(item => <Card/>) }
                           </aside>
                   </section>
                   
