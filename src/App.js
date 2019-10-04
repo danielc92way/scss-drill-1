0555c550c30ab1b8e82463ba1d5e6ed531c437ed
Daniel Corcoran <danielcorcoran@Daniel-Corcorans-Macbook.local>
2019-10-04 12:35:43 +1000

    afasdfasdf

--- src/App.js
+++ src/App.js
@@ -4,21 +4,55 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+          <nav>
+              <div class="container">
+                  <h1 class="title--large">Ipsum in ut ex excepteur labore sint.</h1>
+              </div>
+          </nav>
+
+          <main>
+              <div class="container">
+                  <section class="main-container">
+                          <aside class="left-side container__general">
+                              <article class="card">
+                                  <h3>Dolore labore.</h3>
+                                  <p>Consectetur in dolor deserunt qui sint et nisi magna.</p>
+                              </article>
+                              <article class="card">
+                                  <h3>Dolore labore.</h3>
+                                  <p>Consectetur in dolor deserunt qui sint et nisi magna.</p>
+                              </article>
+                              <article class="card">
+                                  <h3>Dolore labore.</h3>
+                                  <p>Consectetur in dolor deserunt qui sint et nisi magna.</p>
+                              </article>
+                          </aside>
+                          <section class="article-list container__general">
+                              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam eos ipsa voluptate dolorem magni repellat nisi soluta, eaque odit in deleniti dolores, vel facere velit officiis. Nihil hic obcaecati debitis.
+                          </section>
+                          <aside class="right-side container__general">
+                              <article class="card">
+                                  <h3>Dolore labore.</h3>
+                                  <p>Consectetur in dolor deserunt qui sint et nisi magna.</p>
+                              </article>
+                              <article class="card">
+                                  <h3>Dolore labore.</h3>
+                                  <p>Consectetur in dolor deserunt qui sint et nisi magna.</p>
+                              </article>
+                              <article class="card">
+                                  <h3>Dolore labore.</h3>
+                                  <p>Consectetur in dolor deserunt qui sint et nisi magna.</p>
+                              </article>
+                          </aside>
+                  </section>
+                  
+              </div>
+          </main>
+
+          <footer>
+              <h6>Do nisi adipisicing sunt exercitation veniam Lorem excepteur nostrud esse laboris esse eu.</h6>
+          </footer>
     </div>
   );
 }
